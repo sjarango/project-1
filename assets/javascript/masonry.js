adfc0d23438b03aefe6205661b2c336a02843cd3
Jeremiah Stafford <jstafford@slmdevice.com>
2019-09-28 13:13:48 -0400

    code chnges

--- assets/javascript/masonry.js
+++ assets/javascript/masonry.js
@@ -3,17 +3,20 @@
 var apiKey = "&api_key=6ahnRsSlhvpPlehhB0fMzpoCmPoENxdPYX8NLcze";
 var rovCams = [];
 
+
 // FUNCTIONS
 // ==========================================================
 
 
-function rowNum(length) {
-    if (length % 7 !== 0) {
-        rows = Math.floor(length / 7) + 1;
+function rowNum(photos) {
+    var rows = 0;
+    if (photos % 4 !== 0) {
+        rows = Math.floor(photos / 4) + 1;
     }
     else {
-        rows = length % 7;
+        rows = photos % 4;
     }
+    return rows;
 }
 
 function addRovOpts(pull) {
@@ -25,154 +28,132 @@ function addRovOpts(pull) {
         $('#rov').append(opt);
     }
 }
-function addCamOpts(obj) {
-        
-        var camL = Object.keys(obj[g].cameras).length;
-        var keyS = Object.keys(obj[g].cameras);
-        for (var e = 0; e < camL; e++) {
-            var camy = keyS[e];
-            if (obj[g].cameras[camy].length !== 0) {
-                rovCams.push(camy);
+
+function addImage(src, imgDiv) {
+
+    var img = $('<img>');
+    img.addClass('d-block');
+    img.attr('src', src);
+    imgDiv.append(img);
+    $('#images').append(imgDiv);
+}
+// The AJAX function uses the queryURL and GETS the JSON data associated with it.
+// The data then gets stored in the variable called: "NYTData"
+function searchPhotos(url) {
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).done(function (response) {
+
+        var photos = response.photos.length;
+
+        var rows = rowNum(photos);
+
+        var j = 0;
+        console.log(photos);
+        console.log(rows);
+
+        for (i = 0; i < rows; i++) {
+            var imgDiv = $('<div>');
+            if (i == 0) {
+                imgDiv.addClass('carousel-item active');
+            }
+            else {
+                imgDiv.addClass('carousel-item');
+            }
+            for (h = 0; h < 4; h++) {
+                var imgDiv2 = $('<div>');
+                j++;
+                var src = response.photos[j].img_src;
+                imgDiv2.addClass('col-xs-3 col-sm-3 col-md-3');
+                imgDiv.append(imgDiv2);
+                addImage(src, imgDiv);
+
             }
         }
-        for (var h = 0; h < rovCams.length; h++) {
-            var opt = $('<option>');
-            opt.attr('value', rovCams[h]);
-            opt.text(rovCams[h]);
-            $('#camera-select').append(opt);
-        }
-    }
-
-    function addImage(src, imgDiv) {
-
-        var img = $('<img>');
-        img.addClass('d-block');
-        img.attr('src', src);
-        imgDiv.append(img);
-        $('#images').append(imgDiv);
-    }
-    // The AJAX function uses the queryURL and GETS the JSON data associated with it.
-    // The data then gets stored in the variable called: "NYTData"
-    function searchPhotos(url) {
-        $.ajax({
-            url: url,
-            method: "GET"
-        }).done(function (response) {
-
-            var photos = response.photos.length;
-
-            for (i = 0; i < photos; i++) {
-                var src = response.photos[i].img_src;
-                var imgDiv = $('<div>');
-                if (i == 0) {
-                    imgDiv.addClass('carousel-item active col-6');
-                    addImage(src, imgDiv);
-
-                }
-                else {
-                    imgDiv.addClass('carousel-item col-6');
-                    addImage(src, imgDiv);
-
-                }
-            }
-
-
-        });
-    }
-
-    // METHODS ==========================================================
-
-    // // on.("click") function associated with the Search Button
-    $("#run-search").on("click", function (event) {
-        event.preventDefault();
-
-
-        $("#images").empty();
-
-        // Grabbing text the user typed into the search input
-        eDate = $('#datetimepicker6').datetimepicker('viewDate').format('YYYY-MM-DD');
-        cam = $("#camera-select").val();
-        rov = $("#rov").val();
-
-        var searchURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rov + "/photos?" + "earth_date=" + eDate + "&camera=" + cam + apiKey;
-
-
-        searchPhotos(searchURL);
-
     });
-
-    $('#rov').on('change', function () {
-        
-        $("#cams").removeClass('d-none');
-        camPair();
-
-        
+}
 
 
-    });
+// METHODS ==========================================================
+
+// // on.("click") function associated with the Search Button
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
 
 
-    $("#camera-select").on('change', function () {
-        var cam = $("#camera-select").val();
-        $('#calendar').removeClass('d-none');
-        addCamOpts(enabledDates);
-        var dAtes = enabledDates[r].cameras[cam];
+    $("#images").empty();
 
-        console.log(dAtes);
+    // Grabbing text the user typed into the search input
+    var eDate = $("#date").val();
+    var cam = $("#camera-select").val();
+    var rov = $("#rov").val();
 
-        $('#datetimepicker6').datetimepicker({
-            format: 'MM/DD/YYYY',
-            defaultDate: dAtes[0],
-            enabledDates: dAtes
-        });
-    });
+    var searchURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rov + "/photos?" + "earth_date=" + eDate + "&camera=" + cam + apiKey;
 
 
+    searchPhotos(searchURL);
 
-$('#datetimepicker6').datetimepicker({
-    format: 'MM/DD/YYYY',
-    defaultDate: enabledDates[0].cameras.FHAZ[0],
-    enabledDates: enabledDates[0].cameras.FHAZ
+});
+
+$('#rov').on('change', function () {
+
+    $("#cams").removeClass('d-none');
+    camPair();
+
+
+
+
 });
 
 
-function camPair(){
+$("#camera-select").on('change', function () {
+    var cam = $("#camera-select").val();
+    $('#calendar').removeClass('d-none');
+
+    var dAtes = enabledDates[r].cameras[cam];
+    var sDate = moment(dAtes[0]);
+
+
+
+
+});
+
+
+
+
+function camPair() {
     $('#camera-select').empty();
     if ($('#rov').val() == enabledDates[0].roverName.toLowerCase()) {
         r = 0;
-        console.log('r: ' + r);
-        console.log('object:' + enabledDates[0].roverName.toLowerCase());
-        console.log('input value:' + $('#rov').val());
+
     }
     else if ($('#rov').val() == enabledDates[1].roverName.toLowerCase()) {
         r = 1;
-        console.log('r: ' + r);
-        console.log('object:' + enabledDates[1].roverName.toLowerCase());
-        console.log('input value:' + $('#rov').val());
+
     }
     else {
         r = 2;
-        console.log('r: ' + r);
-        console.log('object:' + enabledDates[2].roverName.toLowerCase());
-        console.log('input value:' + $('#rov').val());
+
     }
 
     var camL = Object.keys(enabledDates[r].cameras).length;
     var keyS = Object.keys(enabledDates[r].cameras);
-        console.log(camL);
-        console.log(keyS);    
-    
-    // for (var e = 0; e < camL; e++) {
-        //     var camy = keyS[e];
-        //     if (enabledDates[r].cameras[camy].length !== 0) {
-        //         rovCams.push(camy);
-        //     }
-        // }
-        
-        // for (var h = 0; h < rovCams.length; h++) {
-        //     var opt = $('<option>');
-        //     opt.attr('value', rovCams[h]);
-        //     opt.text(rovCams[h]);
-        //     $('#camera-select').append(opt);
-        // }
+    console.log(camL);
+    console.log(keyS);
+
+
+    for (var e = 0; e < camL; e++) {
+        var camy = keyS[e];
+        if (enabledDates[r].cameras[camy].length !== 0) {
+            rovCams.push(camy);
+        }
+    }
+
+    for (var h = 0; h < rovCams.length; h++) {
+        var opt = $('<option>');
+        opt.attr('value', rovCams[h]);
+        opt.text(rovCams[h]);
+        $('#camera-select').append(opt);
+    }
 }
