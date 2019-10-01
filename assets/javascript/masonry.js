aead7780b24087b904ce635c300e7e0c175ee67d
Jeremiah Stafford <jstafford@slmdevice.com>
2019-10-01 16:22:31 -0400

    code refine

--- assets/javascript/masonry.js
+++ assets/javascript/masonry.js
@@ -1,14 +1,11 @@
 // SETUP VARIABLES
 
 var apiKey = "&api_key=6ahnRsSlhvpPlehhB0fMzpoCmPoENxdPYX8NLcze";
-var rovCams = [];
+
 var intro = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
 
 var cam = $("#camera-select").val();
-$("#rov").empty();
 
-// The AJAX function uses the queryURL and GETS the JSON data associated with it.
-// The data then gets stored in the variable called: "NYTData"
 function searchPhotos(url) {
     $.ajax({
         url: url,
@@ -58,8 +55,9 @@ $('#rov').change(function () {
 });
 
 function camPair() {                                        //This function assigns text value to numerical ordering of object
+    var rovCams = [];
     var rov = $('#rov').val();                              //and then defines the camera selections based on what cameras are 
-    console.log(rov);
+    
     if (rov == enabledDates[0].roverName.toLowerCase()) {   //present in this rover's camera object array.
         r = 0;
     }
@@ -71,12 +69,12 @@ function camPair() {                                        //This function assi
     }
     imgFest.r = r;
 
-    var camL = Object.keys(enabledDates[r].cameras).length;
-    var keyS = Object.keys(enabledDates[r].cameras);
-    for (var e = 0; e < camL; e++) {
+    var camL = Object.keys(enabledDates[r].cameras).length;     //---Given the number of cameras available for the rover
+    var keyS = Object.keys(enabledDates[r].cameras);            //---and the names of the cameras
+    for (var e = 0; e < camL; e++) {                            //---cycle through all the cameras
         var camy = keyS[e];
-        if (enabledDates[r].cameras[camy].length !== 0) {
-            rovCams.push(camy);
+        if (enabledDates[r].cameras[camy].length !== 0) {       //---determine if images exist on that camera
+            rovCams.push(camy);                                 //---push the cameras that have data to the dropdown variable used for camera selection
         }
     }
     for (var h = 0; h < rovCams.length; h++) {
@@ -90,14 +88,14 @@ function camPair() {                                        //This function assi
 //-----Camera Selector-----//
 $("#camera-select").on('change', function () {                      //Not all dates are available for each camera so depending on the 
     $('#camsavail').empty();                                        //rover and camera selection a new date array is assigned.  The big exception is 
+    sessionStorage.removeItem('dates');
     $('#sDate').empty();                                            //Spirit rover.  The dates available for each camera is present in the rover manifest
     $('#eDate').empty();                                            //which is convenient because ,000's of api calls are spared in order to get the full
     var cam = $("#camera-select").val();                            //date array. For the Spirit rover the earth_date is not present in the manifest, but 
                                                                     //martian day(sol) is made available, which is used instead of earth_date.
-    console.log(cam);
-    imgFest.camera = cam;                                           //A new object is created for the date range api iteration
     var r = imgFest.r;
     var dAtes = enabledDates[r].cameras[cam];
+    imgFest.camera = cam;                                           //A new object is created for the date range api iteration
     sessionStorage.setItem("dates", JSON.stringify(dAtes));
 
     for (var f = 0; f < dAtes.length; f++) {               //The camera selection dictates the dates available
@@ -124,12 +122,12 @@ $("#camera-select").on('change', function () {                      //Not all da
 });
 
 //-----Date Selector-----//
-$('#eDate').on('change', function () {
+$('#eDate').on('change', function () {                          //---Once the end date is set, the rover, camera and date arrays can be used to query the api
     var start = $('#sDate').val();
     var ennd = $('#eDate').val();
-    var dateArray = JSON.parse(sessionStorage.getItem("dates"));
-    btw(start, ennd, dateArray);
-    $('jumbotron').removeClass('d-none');
+    var dateArray = JSON.parse(sessionStorage.getItem("dates"));//---This is the array of all the dates possible for this rover & camera pair
+    btw(start, ennd, dateArray);                                //---Find the array of dates selected
+    $('jumbotron').removeClass('d-none');                       //---Show the container that gives basic information about the rover,camera and images.
     var rovName = $('<h3>');
 
 });
@@ -156,9 +154,12 @@ $("#reset-search").on("click", function (event) {
     $('#camera-select').empty();
     $('#rov').empty();
     $('#camsavail').empty();
+    newArray =[];
+    images = [];
     $("#mars").addClass('d-none');
     sessionStorage.removeItem('images');
     sessionStorage.removeItem('dates'); 
+    addRovOpts(roverPull);
 });
 
 var imgFest = [
@@ -174,35 +175,26 @@ var imgFest = [
 var images = [];
 var camname = "";
 var earDate = "";
-//---API Query Function---//
-function getFotos(datearray) {
-    var rov = $("#rov").val();
+
+//---Multiple Image API Query Function---//
+function getFotos(datearray) {                          //--Given the available dates, run an array of calls
+    var rov = $("#rov").val();                          //--use the rover and camera values to run the queries
     var cam = $("#camera-select").val();
+    
     var calls = datearray.map(it => {
         return $.ajax({
             url: intro + rov + "/photos?earth_date=" + it + "&camera=" + cam + apiKey,
             method: "GET",
-
-        }).then(function (response) {
-            
+        }).then(function (response) {  
             images.push(response.photos); 
-            
-
-        });
-        
+       }); 
     });
-    
-
     Promise.all(calls)
         .then(function () {
-            
             sessionStorage.setItem('images', JSON.stringify(images)); 
-
             layImages();
-
         });
-
-}
+    }
 
 
 
@@ -212,32 +204,32 @@ var imgs=[];
 var edtes = [];
 var ids = [];
 
+//---This function flattens the image data and then places the images in carousel HTML---//
 function layImages() {
     var images = JSON.parse(sessionStorage.getItem('images'));
     var srcNum = images.length;
     
-    for (var j = 0; j < srcNum; j++) {
-
+    for (var j = 0; j < srcNum; j++) {                  //--Multiple images can occur on each day--//
+                                                        //'for each day, push each image to a flat image, date and id array'
         for (var l = 0; l < images[j].length;l++){
-            var imgsOD = images[j][l];
+            var imgsOD = images[j][l];                  //imgsOD is 'images of the day' 
             imgs.push(imgsOD.img_src);
             edtes.push(imgsOD.earth_date);
-            ids.push(imgsOD.id);
+            ids.push(imgsOD.id);                        //This should be 3 equal length arrays
             
         }
     }
 
-    for (u = 0;u < images.length;u++){
-            var imgDiv = $('<div>');
-            imgDiv.addClass('carousel-item');
-            var imgDiv2 = $('<div>');
-            imgDiv2.addClass('col-12');
-            var img = $('<img>');
-            img.addClass('center-block');
-            img.attr('src', imgs[u]);
-            var imgDiv3 = $('<div>');
-            imgDiv3.addClass('carousel-caption');
-            var imgH4 = $('<h4>');
+    for (u = 0 ; u < images.length;u++){                //Place the image, date and image id into carousel
+            var imgDiv = $('<div>');                    //<div class="carousel-item">                   (imgDiv)
+            imgDiv.addClass('carousel-item');           //  <div class="col-12">                        (imgDiv2)
+            var imgDiv2 = $('<div>');                   //      <img src="imgs[u]">                     (img)
+            imgDiv2.addClass('col-12');                 //       <div class="carousel-caption">         (imgDiv3)
+            var img = $('<img>');                       //           <h4>Earth Date: edtes[u]</h4>      (imgH4)
+            img.attr('src', imgs[u]);                   //           <p>Image id: ids[u]</p>            (pTag)
+            var imgDiv3 = $('<div>');                   //       </div>
+            imgDiv3.addClass('carousel-caption');       //  </div>
+            var imgH4 = $('<h4>');                      //</div>
             imgH4.text('Earth Date: ' + moment(edtes[u]).format('MM/DD/YY'));
             var pTag = $('<p>');
             pTag.text('Image id: ' + ids[u]);
@@ -250,24 +242,22 @@ function layImages() {
         }
        
    
-    var carItem = document.getElementsByClassName('carousel-item');
+    var carItem = document.getElementsByClassName('carousel-item'); //Find the first carousel item and make it active
     carItem[0].classList.add('active');
 }
 
-
+//--This function finds the dates in the available date array--//
 function btw(start, ennd, dateArray) {
     var diff = moment(ennd).diff(moment(start), 'days');
     dateArray.forEach(function (day, i) {
         if (day >= start == true && day <= ennd == true) {
-
-
             newArray.push(day);
-
         }
         return diff;
     });
 }
 
+//--This function is used for multiple image slide show--// 
 function rowNum(photos) {
     var rows;
     if (photos < 4) {
