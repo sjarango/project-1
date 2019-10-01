bb8cc01dd089ae24855b8d2b465f1dddea15ae42
Jeremiah Stafford <jstafford@slmdevice.com>
2019-10-01 12:37:40 -0400

    carousel fixed with text

--- assets/javascript/masonry.js
+++ assets/javascript/masonry.js
@@ -46,7 +46,7 @@ var newArray = [];
 //---On Change Event Functions---//
 
 //-----Rover Selector-----//
-$('#rov').on('change', function () {
+$('#rov').change(function () {
     $('#camera-select').empty();
     $('#sDate').empty();
     $('#eDate').empty();
@@ -93,7 +93,7 @@ $("#camera-select").on('change', function () {                      //Not all da
     $('#sDate').empty();                                            //Spirit rover.  The dates available for each camera is present in the rover manifest
     $('#eDate').empty();                                            //which is convenient because ,000's of api calls are spared in order to get the full
     var cam = $("#camera-select").val();                            //date array. For the Spirit rover the earth_date is not present in the manifest, but 
-    //martian day(sol) is made available, which is used instead of earth_date.
+                                                                    //martian day(sol) is made available, which is used instead of earth_date.
     console.log(cam);
     imgFest.camera = cam;                                           //A new object is created for the date range api iteration
     var r = imgFest.r;
@@ -101,18 +101,18 @@ $("#camera-select").on('change', function () {                      //Not all da
     sessionStorage.setItem("dates", JSON.stringify(dAtes));
 
     for (var f = 0; f < dAtes.length; f++) {               //The camera selection dictates the dates available
-        var opt = $('<option>');                         //opt = start date
-        var opt2 = $('<option>');                         //opt2 = end date
-        var sDate = dAtes[0];
-        if (f == 0) {
-            opt.attr('placeholder', moment(sDate).format('MM/DD/YY'));
-        }
-
+        var opt = $('<option>');                           //opt = start date
+        var opt2 = $('<option>');                          //opt2 = end date
         opt.attr('value', dAtes[f]);
         opt2.attr('value', dAtes[f]);
-        opt2.attr('placeholder', moment(sDate).add(10, 'days').format('MM/DD/YY'));
+        if (cam !== "Spirit"){
         opt.text(moment(dAtes[f]).format('MM/DD/YY'));
         opt2.text(moment(dAtes[f]).format('MM/DD/YY'));
+        }
+        else{
+            opt.text(dAtes[f]);
+            opt2.text(dAtes[f]);
+        }
         $('#sDate').append(opt);
         $('#eDate').append(opt2);
     }
@@ -129,17 +129,36 @@ $('#eDate').on('change', function () {
     var ennd = $('#eDate').val();
     var dateArray = JSON.parse(sessionStorage.getItem("dates"));
     btw(start, ennd, dateArray);
+    $('jumbotron').removeClass('d-none');
+    var rovName = $('<h3>');
+
 });
 
 //-----Submit Query Button-----//
 $("#run-search").on("click", function (event) {
     event.preventDefault();
     $("#images").empty();
+    $("#mars").removeClass('d-none');
     getFotos(newArray);
 
     // var searchURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rov + "/photos?earth_date=" + eDate + "&camera=" + cam + apiKey;
     // searchPhotos(searchURL);
 
+});
+
+
+//-----Reset Button-----//
+$("#reset-search").on("click", function (event) {
+    event.preventDefault();
+    $('#images').empty();
+    $('#sDate').empty();
+    $('#eDate').empty();
+    $('#camera-select').empty();
+    $('#rov').empty();
+    $('#camsavail').empty();
+    $("#mars").addClass('d-none');
+    sessionStorage.removeItem('images');
+    sessionStorage.removeItem('dates'); 
 });
 
 var imgFest = [
@@ -165,14 +184,19 @@ function getFotos(datearray) {
             method: "GET",
 
         }).then(function (response) {
-            sessionStorage.setItem('images', JSON.stringify(response.photos));
-            // return response.photos;
+            
+            images.push(response.photos); 
+            
 
         });
+        
     });
+    
 
     Promise.all(calls)
         .then(function () {
+            
+            sessionStorage.setItem('images', JSON.stringify(images)); 
 
             layImages();
 
@@ -184,35 +208,48 @@ function getFotos(datearray) {
 
 
 //---Functions---//
+var imgs=[];
+var edtes = [];
+var ids = [];
 
 function layImages() {
     var images = JSON.parse(sessionStorage.getItem('images'));
     var srcNum = images.length;
-    var rows = rowNum(srcNum);
+    
     for (var j = 0; j < srcNum; j++) {
+
+        for (var l = 0; l < images[j].length;l++){
+            var imgsOD = images[j][l];
+            imgs.push(imgsOD.img_src);
+            edtes.push(imgsOD.earth_date);
+            ids.push(imgsOD.id);
+            
+        }
+    }
+
+    for (u = 0;u < images.length;u++){
             var imgDiv = $('<div>');
             imgDiv.addClass('carousel-item');
             var imgDiv2 = $('<div>');
             imgDiv2.addClass('col-12');
-            var src = images[j].img_src;
             var img = $('<img>');
-            // img.addClass('d-block');
-            img.attr('src', src);
+            img.addClass('center-block');
+            img.attr('src', imgs[u]);
             var imgDiv3 = $('<div>');
             imgDiv3.addClass('carousel-caption');
             var imgH4 = $('<h4>');
-            imgH4.text('Earth Date: ' + moment(images[j].earth_date).format('MM/DD/YY'));
+            imgH4.text('Earth Date: ' + moment(edtes[u]).format('MM/DD/YY'));
             var pTag = $('<p>');
-            pTag.text('Image id: ' + images[j].id);
+            pTag.text('Image id: ' + ids[u]);
             imgDiv3.append(imgH4,pTag);
             imgDiv2.append(img,imgDiv3);
             imgDiv.append(imgDiv2);
-
-            
             $('#images').append(imgDiv);
-
+       
+       
         }
- 
+       
+   
     var carItem = document.getElementsByClassName('carousel-item');
     carItem[0].classList.add('active');
 }
